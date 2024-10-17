ec08a8d89b4ba8b005b66ad854cee1931edfd2cb
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-10-17 23:19:34 +0530

    added animations for walking/running

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,3 +1,4 @@
+using System;  // Required for Math.Abs
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,14 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public Animator animator;
     public int maxHealth = 100;
     public int currHealth;
     private float Move;
     public float speed;
     public float jump;
+    public bool jumping = false;
+    private bool facingRight = true;  // Track which direction the player is facing
 
     public HealthBar bar;
 
@@ -21,34 +25,50 @@ public class PlayerScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currHealth = maxHealth;
         bar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
     void Update()
     {
-    
         Move = Input.GetAxisRaw("Horizontal");
-        // rb.AddForce(new Vector2(Move * speed, rb.velocity.y));
         rb.velocity = new Vector2(Move * speed, rb.velocity.y);
-        // Debug.Log(isGrounded());/ß
-        if (Input.GetButtonDown("Jump") && isGrounded())
+
+        // Update animator with velocity values
+        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
+
+        // Update jumping state
+        bool isGroundedNow = isGrounded();
+        animator.SetBool("Jumping", !isGroundedNow);
+
+        if (Input.GetButtonDown("Jump") && isGroundedNow)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+            jumping = true;
             TakeDamage();
         }
+
+        if (isGroundedNow)
+        {
+            jumping = false;
+        }
+
+        // Flip player direction based on movement
+        if (Move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (Move < 0 && facingRight)
+        {
+            Flip();
+        }
     }
+
     public bool isGrounded()
     {
-        if (Physics2D.BoxCast(transform.position, Boxsize, 0, -transform.up, castDistance, BGLayer))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return Physics2D.BoxCast(transform.position, Boxsize, 0, -transform.up, castDistance, BGLayer);
     }
 
     private void OnDrawGizmos()
@@ -56,8 +76,16 @@ public class PlayerScript : MonoBehaviour
         Gizmos.DrawWireCube(transform.position - transform.up * castDistance, Boxsize);
     }
 
-    public void TakeDamage(){
+    public void TakeDamage()
+    {
         currHealth -= 20;
         bar.SetHealth(currHealth);
+    }
+
+    // Flip the player by rotating on the Y-axis
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);  // Rotate the player by 180 degrees around the Y-axis
     }
 }
