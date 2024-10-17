ba0a52076c92fc7da687183a3deeccecf95159da
Trijay1709 <trijaypatel1709@gmail.com>
2024-10-18 03:25:52 +0530

    added attack logic

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -20,9 +20,18 @@ public class PlayerScript : MonoBehaviour
     public HealthBar bar;
 
     public Vector2 Boxsize;
+    public GameObject attackPoint;
     public float castDistance;
+    public float radius;
 
     public LayerMask BGLayer;
+    public LayerMask Player;
+    public KeyCode moveLeftKey;
+    public KeyCode moveRightKey;
+    public KeyCode jumpKey;
+    public KeyCode attackKey;
+
+    public bool InitialRight;
 
     void Start()
     {
@@ -30,11 +39,30 @@ public class PlayerScript : MonoBehaviour
         animator = GetComponent<Animator>();
         currHealth = maxHealth;
         bar.SetMaxHealth(maxHealth);
+        if (!InitialRight)
+        {
+            Flip();
+        }
     }
+
 
     void Update()
     {
-        Move = Input.GetAxisRaw("Horizontal");
+
+        // Handle movement
+        if (Input.GetKey(moveLeftKey))
+        {
+            Move = -1;
+        }
+        else if (Input.GetKey(moveRightKey))
+        {
+            Move = 1;
+        }
+        else
+        {
+            Move = 0;
+        }
+
         rb.velocity = new Vector2(Move * speed, rb.velocity.y);
 
         // Update animator with horizontal velocity
@@ -51,18 +79,22 @@ public class PlayerScript : MonoBehaviour
         }
 
         // Handle jump
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        if (Input.GetKeyDown(jumpKey) && isGrounded())
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
-            TakeDamage();
         }
 
         // Handle attack with cooldown
-        if (Input.GetKeyDown(KeyCode.W) && canAttack)
+        if (Input.GetKeyDown(attackKey) && canAttack)
         {
             Attack();
         }
+
     }
+    // Continuously update the attackPoint's position based on which direction the player is facing
+
+
+
 
     public bool isGrounded()
     {
@@ -71,7 +103,17 @@ public class PlayerScript : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        // Draw the box that checks for ground (isGrounded)
+        Gizmos.color = Color.green;
         Gizmos.DrawWireCube(transform.position - transform.up * castDistance, Boxsize);
+
+        // Draw the attack range at the attackPoint
+        if (attackPoint != null)
+        {
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPoint.transform.position, radius);
+        }
     }
 
     public void TakeDamage()
@@ -83,13 +125,22 @@ public class PlayerScript : MonoBehaviour
     // Flip the player by rotating on the Y-axis
     private void Flip()
     {
+        Debug.Log(attackPoint.transform.position);
         facingRight = !facingRight;
+
         transform.Rotate(0f, 180f, 0f);  // Rotate the player by 180 degrees around the Y-axis
     }
 
     // Trigger the attack animation
     private void Attack()
     {
+        Collider2D enemy = Physics2D.OverlapCircle(attackPoint.transform.position, radius, Player);
+        if (enemy)
+        {
+
+            Debug.Log("Enemy Hit");
+            enemy.GetComponent<PlayerScript>().TakeDamage();
+        }
         canAttack = false;  // Disable further attacks during cooldown
         animator.SetBool("isAttacking", true);  // Start the attack animation
         Invoke("StopAttack", 0.2f);  // Stop the attack animation after 0.2 seconds
